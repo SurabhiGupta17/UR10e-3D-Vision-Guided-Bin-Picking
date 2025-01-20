2cf03b11a1122467aacbb94cce979a9b4af1621d
SurabhiGupta17 <surabhi.gupta1617@gmail.com>
2025-01-20 15:38:37 +0530

    Add launch file

---
+++ src/gazebo/launch/bin_picking.launch.py
@@ -0,0 +1,124 @@
+import os
+import xacro
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import ExecuteProcess, TimerAction
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+
+    ur_desc_path = get_package_share_directory('ur_description')
+
+    env = {
+        'GZ_SIM_SYSTEM_PLUGIN_PATH': ':'.join([
+            os.environ.get('GZ_SIM_SYSTEM_PLUGIN_PATH', ''),
+            os.path.join(get_package_share_directory('ros_gz_sim'), 'lib')
+        ]),
+        'IGN_GAZEBO_SYSTEM_PLUGIN_PATH': ':'.join([
+            os.environ.get('IGN_GAZEBO_SYSTEM_PLUGIN_PATH', ''),
+            os.path.join(get_package_share_directory('ros_gz_sim'), 'lib')
+        ]),
+        'GZ_SIM_RESOURCE_PATH': ':'.join([
+            os.environ.get('GZ_SIM_RESOURCE_PATH', ''),
+            os.path.dirname(ur_desc_path)
+        ])
+    }
+
+    robot_desc_pkg_dir = get_package_share_directory('ur_description')
+    gazebo_pkg_dir = get_package_share_directory('gazebo')
+    
+    urdf_xacro_path = os.path.join(robot_desc_pkg_dir, 'urdf', 'ur.urdf.xacro')
+    urdf_output_path = os.path.join(robot_desc_pkg_dir, 'urdf', 'generated.urdf')
+    
+    xacro_args = {
+    'name': 'ur',
+    'ur_type': 'ur10e', 
+    'tf_prefix': '',
+    'force_abs_paths': 'true'
+    }
+
+    xacro_cmd = [urdf_xacro_path]
+    for key, value in xacro_args.items():
+        xacro_cmd.extend([f'{key}:={value}'])
+
+    urdf_content = xacro.process_file(
+        urdf_xacro_path,
+        mappings={
+            'name': 'ur',
+            'ur_type': 'ur10e',
+            'tf_prefix': '',
+            'force_abs_paths': 'true'
+        }
+    ).toxml()
+    os.makedirs(os.path.dirname(urdf_output_path), exist_ok=True)
+    with open(urdf_output_path, 'w') as urdf_file:
+        urdf_file.write(urdf_content)
+
+    world_file_path = os.path.join(gazebo_pkg_dir, 'worlds', 'bin_picking_world.sdf')
+
+    robot_description = {'robot_description': urdf_content}
+
+    robot_state_pub_node = Node(                                                                                
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        output="both",
+        parameters=[robot_description],
+        # namespace='robot_arm',
+        # remappings=[('/tf', '/robot_arm/tf'), ('/tf_static', '/robot_arm/tf_static'),]
+    )
+
+    # joint_state_broadcaster_spawner = Node(
+    #     package="controller_manager",
+    #     executable="spawner",
+    #     arguments=["joint_state_broadcaster", "--controller-manager", "/robot_arm/controller_manager"],
+    #     namespace='robot_arm',
+    # )
+
+    # joint_controller_gui = Node(
+    #     package="joint_controller_gui",
+    #     executable="joint_controller_gui",
+    #     namespace='robot_arm',
+    #     remappings=[('/position_controller', '/robot_arm/position_controller'), ('/position_controller/commands', '/robot_arm/position_controller/commands')]
+    # )
+
+    # robot_controller_spawner = Node(
+    #     package="controller_manager",
+    #     executable="spawner",
+    #     arguments=["position_controller", "--controller-manager", "/robot_arm/controller_manager"],
+    #     namespace='robot_arm',
+    # )
+
+    gazebo_process = ExecuteProcess(
+    cmd=['gz', 'sim', '-r', world_file_path],
+    output='screen',
+    additional_env=env,
+    )
+
+    spawn_robot_arm = ExecuteProcess(
+        cmd=['gz', 'service', '-s', '/world/empty/create',
+             '--reqtype', 'gz.msgs.EntityFactory',
+             '--reptype', 'gz.msgs.Boolean',
+             '--timeout', '2000',
+             '--req', f'sdf_filename: "{urdf_output_path}", name: "bin_picking_robot", pose: {{position: {{x: 0, y: 0, z: 0}}}}'],
+        output='screen'
+    )
+
+    return LaunchDescription([
+        gazebo_process,
+        TimerAction(
+            period=2.0,
+            actions=[robot_state_pub_node]
+        ),
+        TimerAction(
+            period=4.0,
+            actions=[spawn_robot_arm]
+        ),
+        # TimerAction(
+        #     period=5.0,
+        #     actions=[joint_state_broadcaster_spawner]
+        # ),
+        # TimerAction(
+        #     period=6.0,
+        #     actions=[robot_controller_spawner]
+        # ),
+    ])
